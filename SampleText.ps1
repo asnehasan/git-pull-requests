06f7428c09a735b27ebde2dfcddfaed2cf83954e
asnehasan <151644742+asnehasan@users.noreply.github.com>
2024-01-18 22:42:11 -0600

    Update SampleText.ps1

--- SampleText.ps1
+++ SampleText.ps1
@@ -6,7 +6,4 @@ write-Host "added by LB"
 ## riga superiore aggiunta da LB
 Write-Host "adding to git pull tutorial"
 ## changes
-Write-Host "Hello Tech World"
-Write-Host "Adding new content"
-write-host "git pull tutorial sample"
 ##some change
